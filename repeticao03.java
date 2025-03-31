8c5223d653362795ad2285d82dd583e44781aeec
LariQueiroz <larissa.mariz1a@gmail.com>
2025-03-31 13:58:35 -0300

    eu gosto de estudar logaritmo

---
+++ repeticao03.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * 
+Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+
+*******************************************************************************/
+
+
+public class repeticao03 {
+public static void main(String[] args) {
+        
+        
+        for (int i = 1; i <= 10; i++) {
+        System.out.println("Eu gosto de estudar algoritmos");
+        
+       }
+    }
+}
