91af74923f2bcfc470c1127566ca27843ff2f0e8
LariQueiroz <larissa.mariz1a@gmail.com>
2025-04-14 14:16:42 -0300

    aprendendo vetor (array)

---
+++ exemploVetor.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ Exemplo: Utilizando Array e visualisando a saída de dados
+ *******************************************************************************/
+ 
+ 
+ public class exemploVetor {
+     public static void main(String[] args) {
+       int[] numeros = new int[5];
+       
+       numeros[0] = 10;
+       numeros[1] = 20;
+       numeros[2] = 30;
+       numeros[3] = 40;
+       numeros[4] = 50;
+       
+       for(int i=0; i<numeros.length; i++){
+           System.out.println("O " + (i+1) + "º número é:" + numeros[i]);
+       }
+     }
+ }
