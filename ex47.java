e16ccccf7821bff0c002c98394ec1031a1be0528
LariQueiroz <larissa.mariz1a@gmail.com>
2025-04-16 07:44:19 -0300

    escrever 20 números em ordem inversa

---
+++ ex47.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+47. Desenvolva um algoritmo que leia um conjunto de 20 valores inteiros,
+guarde-os em um vetor e escreva-os em ordem inversa.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class ex47 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[20];
+
+       
+        System.out.println("Digite 20 números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        
+        System.out.println("\nNúmeros em ordem inversa:");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
